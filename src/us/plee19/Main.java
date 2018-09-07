ee0e9bf2d84a7e073214b246188774db39628ff6
plee19 <42356593+plee19@users.noreply.github.com>
2018-09-07 17:28:33 -0500

    Sorting and counting works for input files, haven't been able to get two-file match to work

--- src/us/plee19/Main.java
+++ src/us/plee19/Main.java
@@ -1,8 +1,43 @@
 package us.plee19;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
+    private final static FileInput placesFile = new FileInput("places.csv");
+    private final static FileInput stuffFile = new FileInput("stuff.csv");
+
     public static void main(String[] args) {
-	// write your code here
+        String line;
+        String[] fields;
+        ArrayList<String> countriesList = new ArrayList<>();
+        countriesList = genArrayList(placesFile);
+        Collections.sort(countriesList);
+        System.out.println(Collections.frequency(countriesList, "India"));
+
+        // Attempt at two file match WIP
+        while ((line = placesFile.fileReadLine()) != null) {
+            fields = line.split(",");
+
+        }
     }
+
+    public static ArrayList<String> genArrayList(FileInput file) {
+        String line;
+        String[] fields;
+        ArrayList<String> countriesList = new ArrayList<>();
+
+        while ((line = file.fileReadLine()) != null) {
+            fields = line.split(",");
+            countriesList.add(fields[0]);
+        }
+        return countriesList;
+    }
+
+
 }
+
+
